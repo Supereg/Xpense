27506ebc040032eda6d783157acce15d21efad15
Andreas Bauer <andi.bauer@tum.de>
2023-03-30 23:28:10 +0200

    Add tests for s17 e1

--- XpenseTest/XpenseTest.swift
+++ XpenseTest/XpenseTest.swift
@@ -1,201 +1,93 @@
 //
-//  XpenseTests.swift
-//  Xpense
+// Created by Andreas Bauer on 30.03.23.
 //
 
+import Foundation
+
 import XCTest
-import ViewInspector
-import SwiftUI
 @testable import Xpense
 @testable import XpenseModel
 
+extension Tag {
+    static func accept(visitor: XpenseTest.CodableTestVisitor) throws {
+        // we use this as a workaround to check if Codable is implemented!
+        try visitor.visit(Self.self)
+    }
+}
+
+protocol TransactionWithTag {
+    var tags: [Tag]? { get }
+}
+
+extension TransactionWithTag {
+    var tags: [Tag]? {
+        // see testTransactionTagProperty
+        XCTFail("Transaction doesn't properly implement the tags property!")
+        return nil
+    }
+}
+
+protocol TagInitializer {
+    init(id: UUID, name: String, color: String, url: URL?)
+}
+
+extension Transaction: TransactionWithTag {}
+
 class XpenseTest: XCTestCase {
-    
+    static let string = "{\"id\": \"E3F91FD8-B9D7-41E8-9E4A-C704EF2011CE\", \"name\": \"food\", \"color\": \"green\"}"
+    static let string_with_url = "{\"id\": \"E3F91FD8-B9D7-41E8-9E4A-C704EF2011CE\", \"name\": \"food\", \"color\": \"green\", \"icon_url\": \"https://webstockreview.net/images/clipart-food-simple-14.png\"}"
+
     override func setUp() {
         super.setUp()
     }
-    
-    func testRefresh() async {
-        let loadAccountsCalled = expectation(description: "loadAccounts() runs immediately")
-        let loadTransactionsCalled = expectation(description: "loadTransactions() runs immediately")
-        
-        let model = Model_RefreshMock(
-            loadAccounts: {
-                loadAccountsCalled.fulfill()
-                try? await Task.sleep(nanoseconds: 100_000_000)
-                return []
-            },
-            loadTransactions: {
-                loadTransactionsCalled.fulfill()
-                try? await Task.sleep(nanoseconds: 100_000_000)
-                return []
-            },
-            refresh: {},
-            refreshTransactions: {}
-        )
-        
-        async let refresh: () = model.refresh()
-        
-        let _ = XCTWaiter.wait(
-            for: [loadAccountsCalled, loadTransactionsCalled],
-            timeout: 5 / 100
-        )
-        
-        await refresh
-    }
-    
-    func testRefreshCallsRemoved() async {
-        let refreshCalled = expectation(description: "refresh() is not called by signUp() nor login()")
-        refreshCalled.isInverted = true
-        
-        let restfulModel = RestfulModel_SignUpLoginMock(
-            refresh: {
-                refreshCalled.fulfill()
-            }
-        )
-        
-        await restfulModel.signUp("", password: "")
-        await restfulModel.login("", password: "")
-    
-        await waitForExpectations(timeout: 0)
-    }
-    
-    func testSendRequest() async {
-        let successRequest = NetworkManager.urlRequest("GET", url: URL(string: "https://dummyjson.com/products/1")!)
-        let failureRequest = NetworkManager.urlRequest("GET", url: URL(string: "https://googlee.de")!)
-        
-        do {
-            let _: String = try await NetworkManager.sendRequestAsync(successRequest)
-        } catch DecodingError.typeMismatch {
-            // nop
-        } catch {
-            XCTFail("Request should have succeeded but failed \(error)")
-        }
-        
-        do {
-            let _: String = try await NetworkManager.sendRequestAsync(failureRequest)
-            XCTFail("Request should have failed but didn't")
-        } catch DecodingError.typeMismatch {
-            XCTFail("Request should have failed but didn't")
-        } catch {
-            // nop
-        }
-    }
-    
-    @MainActor
-    func testAccountsOverview() async {
-        var callsToRefresh = 0
-        
-        let model = Model_RefreshMock(
-            loadAccounts: { return [] },
-            loadTransactions: { return [] },
-            refresh: {
-                callsToRefresh += 1
-            },
-            refreshTransactions: {}
-        )
-        
-        var sut = AccountsOverview()
-            .environmentObject(model as Model)
-        
-        ViewHosting.host(view: sut)
-        ViewHosting.host(view: sut)
-        
-        try? await Task.sleep(for: Duration.milliseconds(500))
-        
-        if callsToRefresh == 0 {
-            XCTFail("Initial refresh not called by AccountsOverview")
-        } else if callsToRefresh > 1 {
-            XCTFail("Multiple calls to refresh by AccountsOverview")
-        }
-    }
-    
-    @MainActor
-    func testTransactionsOverview() async {
-        var callsToRefreshTransactions = 0
-        
-        let model = Model_RefreshMock(
-            loadAccounts: { return [] },
-            loadTransactions: { return [] },
-            refresh: {},
-            refreshTransactions: {
-                callsToRefreshTransactions += 1
-            }
-        )
-        
-        let sut = await TransactionsOverview()
-            .environmentObject(model as Model)
-        
-        ViewHosting.host(view: sut)
-        ViewHosting.host(view: sut)
-        
-        try? await Task.sleep(for: Duration.milliseconds(500))
-        
-        if callsToRefreshTransactions == 0 {
-            XCTFail("Initial refreshTransactions() not called by TransactionsOverview")
-        } else if callsToRefreshTransactions > 1 {
-            XCTFail("Multiple calls to refreshTransactions() by TransactionsOverview")
-        }
-    }
-}
 
-class Model_RefreshMock: Model {
-    let loadAccounts: () async -> [Account]
-    let loadTransactions: () async -> [XpenseModel.Transaction]
-    let refresh: () async -> Void
-    let refreshTransactions: () async -> Void
-    
-    init(
-        loadAccounts: @escaping () async -> [Account],
-        loadTransactions: @escaping () async -> [XpenseModel.Transaction],
-        refresh: @escaping () async -> Void,
-        refreshTransactions: @escaping () async -> Void
-    ) {
-        self.loadAccounts = loadAccounts
-        self.loadTransactions = loadTransactions
-        self.refresh = refresh
-        self.refreshTransactions = refreshTransactions
-    }
-    
-    override func loadAccounts() async throws -> [Account] {
-        await loadAccounts()
-    }
-    
-    override func loadTransactions() async throws -> [XpenseModel.Transaction] {
-        await loadTransactions()
-    }
-    
-    override func refresh() async {
-        self.didInitialAccountsRefresh = true
-        await refresh()
-    }
-    
-    override func refreshTransactions() async {
-        self.didInitialTransactionsRefresh = true
-        await refreshTransactions()
-    }
-}
+    struct CodableTestVisitor {
+        let string: String
 
-class RestfulModel_SignUpLoginMock: RestfulModel {
-    let refresh: () async -> Void
-    
-    init(
-        refresh: @escaping () async -> Void
-    ) {
-        self.refresh = refresh
+        func visit<T: Equatable>(_ object: T.Type = T.self) throws {
+            XCTFail("\(T.self) doesn't conform to Codable!")
+        }
+
+        func visit<T: Codable & Equatable>(_ object: T.Type = T.self) throws {
+            let encoder = JSONEncoder()
+            let decoder = JSONDecoder()
+
+            let tag = try decoder.decode(T.self, from: string.data(using: .utf8)!)
+            let data = try encoder.encode(tag)
+            let tag0 = try decoder.decode(T.self, from: data)
+
+            XCTAssertEqual(tag, tag0, "Codable implementation doesn't work properly!")
+        }
     }
-    
-    @objc
-    override func sendSignUpRequest(_ name: String, password: String) async throws {
-        // nop
+
+    // part 1
+    func testTagCodable() throws {
+        let visitor = CodableTestVisitor(string: XpenseTest.string)
+        XCTAssertNoThrow(try Tag.accept(visitor: visitor), "Unexpected error thrown on Codable implementation! Are any properties missing?")
     }
-    
-    @objc
-    override func sendLoginRequest(_ name: String, password: String) async throws {
-        // nop
+
+    // part 1
+    func testTransactionTagProperty() {
+        let transaction = Transaction(amount: 2, description: "description", account: UUID())
+        XCTAssertEqual(transaction.tags, nil, "Unexpected value found in tags!")
     }
-    
-    override func refresh() async {
-        await refresh()
+
+    // part 2
+    func testIconsWithCodingKeys() throws {
+        let visitor = CodableTestVisitor(string: XpenseTest.string_with_url)
+        XCTAssertNoThrow(try Tag.accept(visitor: visitor), "Unexpected error thrown on Codable implementation! Are any properties missing?")
+    }
+
+    // part 3
+    func testEncodeAndDecodeMethods() throws {
+        let tag = try Tag.decodeJSON(from: "[\(XpenseTest.string)]".data(using: .utf8)!)
+        let data = try Tag.encodeJSON(from: tag)
+        let tag0 = try Tag.decodeJSON(from: data)
+        XCTAssertEqual(tag, tag0)
+
+        let tag_url = try Tag.decodeJSON(from: "[\(XpenseTest.string_with_url)]".data(using: .utf8)!)
+        let data_url = try Tag.encodeJSON(from: tag_url)
+        let tag0_url = try Tag.decodeJSON(from: data_url)
+        XCTAssertEqual(tag_url, tag0_url)
     }
 }
